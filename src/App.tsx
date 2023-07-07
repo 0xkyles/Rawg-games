b686369a613a3ad4c75331e7de99792897db4179
0xkyles <kylesyellow@gmail.com>
2023-07-07 23:47:14 +0100

    Adding a responsive base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
     return (
         <div className="App">
-            <Button colorScheme="blue">Button</Button>
+            <Grid
+                templateAreas={{
+                    base: `"header" "main"`,
+                    lg: `"header header" "aside main"`,
+                }}
+            >
+                <GridItem area="header" colSpan={2} bg="blue">
+                    Header
+                </GridItem>
+                <Show above="lg">
+                    <GridItem area="aside" bg="orange">
+                        Nav
+                    </GridItem>
+                </Show>
+                <GridItem area="main" bg="red">
+                    Main
+                </GridItem>
+            </Grid>
         </div>
     );
 }
