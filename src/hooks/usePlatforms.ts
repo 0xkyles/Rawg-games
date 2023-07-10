d7c251b83c9ff5b0d68df78e0c22828cdc7d0bdb
0xkyles <kylesyellow@gmail.com>
2023-07-11 00:39:29 +0100

    Adding static data while waiting for platforms to be fetched

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,12 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import platformService, {
     CACHE_KEY_PLATFORM,
 } from "../services/platformService";
+import platforms from "../data/platforms";
 
 const usePlatforms = () =>
     useQuery({
         queryKey: CACHE_KEY_PLATFORM,
         queryFn: platformService.getAll,
         staleTime: 24 * 60 * 60 * 1000,
+        initialData: platforms,
     });
 
 export default usePlatforms;
