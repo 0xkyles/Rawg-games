ab498fae2fab39dfbb88bf904d83ef431ead0062
0xkyles <kylesyellow@gmail.com>
2023-07-14 21:13:48 +0100

    Fixed search bar

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -2,6 +2,7 @@ import { Image, Stack } from "@chakra-ui/react";
 import logo from "../../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -12,7 +13,15 @@ const Navbar = () => {
             paddingX="20px"
             paddingY="10px"
         >
-            <Image src={logo} alt="Logo" boxSize="60px" rounded="full" />
+            <Link to="/">
+                <Image
+                    src={logo}
+                    alt="Logo"
+                    boxSize="60px"
+                    objectFit="cover"
+                    rounded="md"
+                />
+            </Link>
             <SearchInput />
             <ColorModeSwitch />
         </Stack>
--- src/components/navigation/SearchInput.tsx
+++ src/components/navigation/SearchInput.tsx
@@ -2,17 +2,22 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import { useRef } from "react";
 import useGameQuery from "../../stores/gameQueryStore";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
     const ref = useRef<HTMLInputElement>(null);
     const setSearch = useGameQuery((s) => s.setSearch);
+    const navigate = useNavigate();
 
     return (
         <form
             style={{ width: "100%" }}
             onSubmit={(event) => {
                 event.preventDefault();
-                if (ref.current) setSearch(ref.current.value);
+                if (ref.current) {
+                    setSearch(ref.current.value);
+                    navigate("/");
+                }
             }}
         >
             <InputGroup>
